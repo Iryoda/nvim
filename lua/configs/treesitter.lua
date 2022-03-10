324d39f94c4fb0bbeb6559aaabaab71fffa3161d
Augusto Iryoda <iryodap@gmail.com>
2022-03-10 20:12:44 -0300

    :tada: update: treesitter

--- lua/configs/treesitter.lua
+++ lua/configs/treesitter.lua
@@ -1,91 +1,90 @@
-require "nvim-treesitter.configs".setup {
-  highlight = {
-    enable = true,
-    use_languagetree=true
-  },
-  indent = {
-    enable = true,
-    disable = {}
-  },
-  incremental_selection = {
-    enable = true,
-    keymaps = {
-      init_selection = "<C-n>",
-      node_incremental = "<C-n>",
-      scope_incremental = "<C-s>",
-      node_decremental = "<C-r>",
-    },
-  },
-  query_linter = {
-    enable = true,
-    use_virtual_text = true,
-    lint_events = { "BufWrite", "CursorHold" },
-  },
-  textsubjects = {
-    enable = true,
-    keymaps = {
-      ["."] = "textsubjects-smart",
-      [";"] = "textsubjects-container-outer",
-    },
-  },
-  playground = {
-    enable = true,
-    disable = {},
-    updatetime = 25, -- Debounced time for highlighting nodes in the playground from source code
-    persist_queries = true, -- Whether the query persists across vim sessions
-    keybindings = {
-      toggle_query_editor = "o",
-      toggle_hl_groups = "i",
-      toggle_injected_languages = "t",
-      toggle_anonymous_nodes = "a",
-      toggle_language_display = "I",
-      focus_language = "f",
-      unfocus_language = "F",
-      update = "R",
-      goto_node = "<cr>",
-      show_help = "?",
-    },
-  },
-  textobjects = {
-    select = {
-      enable = true,
-      lookahead = true,
-      keymaps = {
-        -- You can use the capture groups defined in textobjects.scm
-        ["af"] = "@function.outer",
-        ["if"] = "@function.inner",
-        ["ac"] = "@class.outer",
-        ["ic"] = "@class.inner",
-      },
-    },
-    move = {
-      enable = true,
-      set_jumps = true, -- whether to set jumps in the jumplist
-      goto_next_start = { ["]m"] = "@function.outer", ["]]"] = "@class.outer" },
-      goto_next_end = { ["]M"] = "@function.outer", ["]["] = "@class.outer" },
-      goto_previous_start = { ["[m"] = "@function.outer", ["[["] = "@class.outer" },
-      goto_previous_end = { ["[M"] = "@function.outer", ["[]"] = "@class.outer" },
-    },
-    lsp_interop = {
-      enable = true,
-      peek_definition_code = {
-        ["gD"] = "@function.outer",
-      },
-    },
-  },
-  ensure_installed = {
-    "tsx",
-    "json",
-    "html",
-    "scss",
-    "python",
-    "cpp",
-    "c",
-    "rust"
-  }
-}
+require("nvim-treesitter.configs").setup({
+	highlight = {
+		enable = true,
+		use_languagetree = true,
+	},
+	indent = {
+		enable = true,
+		disable = {},
+	},
+	incremental_selection = {
+		enable = true,
+		keymaps = {
+			init_selection = "<C-n>",
+			node_incremental = "<C-n>",
+			scope_incremental = "<C-s>",
+			node_decremental = "<C-r>",
+		},
+	},
+	query_linter = {
+		enable = true,
+		use_virtual_text = true,
+		lint_events = { "BufWrite", "CursorHold" },
+	},
+	textsubjects = {
+		enable = true,
+		keymaps = {
+			["."] = "textsubjects-smart",
+			[";"] = "textsubjects-container-outer",
+		},
+	},
+	playground = {
+		enable = true,
+		disable = {},
+		updatetime = 25, -- Debounced time for highlighting nodes in the playground from source code
+		persist_queries = true, -- Whether the query persists across vim sessions
+		keybindings = {
+			toggle_query_editor = "o",
+			toggle_hl_groups = "i",
+			toggle_injected_languages = "t",
+			toggle_anonymous_nodes = "a",
+			toggle_language_display = "I",
+			focus_language = "f",
+			unfocus_language = "F",
+			update = "R",
+			goto_node = "<cr>",
+			show_help = "?",
+		},
+	},
+	textobjects = {
+		select = {
+			enable = true,
+			lookahead = true,
+			keymaps = {
+				-- You can use the capture groups defined in textobjects.scm
+				["af"] = "@function.outer",
+				["if"] = "@function.inner",
+				["ac"] = "@class.outer",
+				["ic"] = "@class.inner",
+			},
+		},
+		move = {
+			enable = true,
+			set_jumps = true, -- whether to set jumps in the jumplist
+			goto_next_start = { ["]m"] = "@function.outer", ["]]"] = "@class.outer" },
+			goto_next_end = { ["]M"] = "@function.outer", ["]["] = "@class.outer" },
+			goto_previous_start = { ["[m"] = "@function.outer", ["[["] = "@class.outer" },
+			goto_previous_end = { ["[M"] = "@function.outer", ["[]"] = "@class.outer" },
+		},
+		lsp_interop = {
+			enable = true,
+			peek_definition_code = {
+				["gD"] = "@function.outer",
+			},
+		},
+	},
+	ensure_installed = {
+		"tsx",
+		"json",
+		"html",
+		"scss",
+		"python",
+		"cpp",
+		"c",
+		"rust",
+	},
+})
 
-local parser_config = require "nvim-treesitter.parsers".get_parser_configs()
+local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
 
-parser_config.tsx.used_by = {"javascript", "typescript.tsx"}
-
+parser_config.tsx.filetype_to_parsername = { "javascript", "typescript.tsx" }
